72cc481fd074974a025c66a21012e5ad529bb962
Gabriel Cavalheiro <gabriel.cavalheiro@nexxera.com>
2024-03-08 06:16:07 -0300

    [main] refactor: fixing webhook endpoint

--- includes/class-wc-gateway-nixpay.php
+++ includes/class-wc-gateway-nixpay.php
@@ -105,7 +105,6 @@ class WC_Gateway_NixPay extends WC_Payment_Gateway
         // Actions.
         add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
         add_action('woocommerce_scheduled_subscription_payment_nixpay', array($this, 'process_subscription_payment'), 10, 2);
-        add_action('woocommerce_api_' . strtolower(self::WEBHOOK_ENDPOINT), [$this, 'webhook']);
     }
 
     /**
@@ -181,13 +180,11 @@ class WC_Gateway_NixPay extends WC_Payment_Gateway
 
         $zip_code = str_replace('-', '', $order->get_billing_postcode());
 
-        $merchant_order_id = wp_generate_uuid4();
-
         $payload = array(
             'merchantOrderId' => "woocommerceOrder-$order_id",
             'transactionType' => 1,
-            'callbackUrl' => 'https://staging.housemag.top/wc-api/nix-pay-credit-webhook',
-            'returnUrl' => 'https://staging.housemag.top/wc-api/nix-pay-credit-webhook',
+            'callbackUrl' => home_url('/wc-api/nix-pay-credit-webhook', 'https'),
+            'returnUrl' => home_url('/wc-api/nix-pay-credit-webhook', 'https'),
             'customer' => array(
                 "tag" => $order->get_formatted_billing_full_name(),
                 "name" => $order->get_formatted_billing_full_name(),
@@ -333,41 +330,6 @@ class WC_Gateway_NixPay extends WC_Payment_Gateway
         $decoded_body = json_decode(wp_remote_retrieve_body($response));
 
         return $decoded_body->access_token;
-
-    }
-
-    public function webhook(): void
-    {
-        error_log(print_r($_REQUEST, true));
-        $postData = file_get_contents('php://input');
-        error_log($postData);
-        $requestData = json_decode($postData);
-        error_log($requestData);
-
-        $order_id = explode('-', $requestData->merchantOrderId)[1];
-        error_log("Webhook receivied for order_id: $order_id");
-
-        $order = wc_get_order($order_id);
-
-        $matchPattern = [
-            1 => 'wc-pending',
-            2 => 'wc-completed',
-            3 => 'wc-cancelled',
-            5 => 'wc-refunded',
-            6 => 'wc-refunded',
-            7 => 'wc-failed',
-            8 => 'wc-failed',
-            9 => 'wc-failed',
-        ];
-
-        $newStatus = $matchPattern[$requestData->payment->paymentStatus];
-        if ($newStatus == 'wc-completed') {
-            $order->payment_complete();
-        }
-        $order->update_status($newStatus);
-
-        error_log("Order $order_id status updated to $newStatus");
-
 
     }
 
